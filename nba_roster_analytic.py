dc04e128053d8c4fdc55350b114d0c954bcfb265
hicala <calderon.hila@gmail.com>
2020-11-04 10:17:07 -0500

    new commit

--- nba_roster_analytic.py
+++ nba_roster_analytic.py
@@ -3,20 +3,15 @@
 
 # # Web Scraping using Beautiful Soup
 # 
+
 # ## Summary
+# 
+# This study is part of a serie of statistical analysis in the composition and salary earned by main and key players in the NBA.
 # 
 # I am using Beautiful Soup for the this Python app. Beautiful Soup is a Python library for parsing data out of HTML and XML files (aka webpages). It works with your favorite parser to provide idiomatic ways of navigating, searching, and modifying the parse tree. 
 # 
-# The major concept with Beautiful Soup is that it allows you to access elements of your page by following the CSS structures, such as grabbing all links, all headers, specific classes, or more. It is a powerful library. Once we grab elements, Python makes it easy to write the elements or relevant components of the elements into other files, such as a CSV, that can be stored in a database or opened in other software.
-# 
 # The data I used came from Atlanta Hawks Roster. Reference: https://www.espn.com/nba/team/roster/_/name/atl/atlanta-hawks
-# 
-# ## Main goal
-# 
-# + To access all of the content from the source code of the webpage with Python
-# + Parse and extract data. 
-# + Save the info in CSV file for further analysis.
-# 
+
 # ## Methodology
 # 
 # 1. Import Modules
@@ -31,49 +26,67 @@
 #     + When complete, close the file
 # 6. The output file in CSV format.
 # 
+
+# ## Main goal
+# 
+# + To access all of the content from the source code of the webpage with Python
+# + Parse and extract data. 
+# + Save the info in CSV file for further analysis.
+
+# 
 # ## Data info extracted:
 # 
 # Name, POS ,Age ,HT ,WT ,College and Salary of Team Roster
 
+# If you don't have Beautiful Soup, install with 'conda install beautifulsoup' in terminal.
+# 
+# Python requires us to explicitly load the libraries that we want to use:
+
 # In[14]:
 
 
-# If you don't have Beautiful Soup, install with 'conda install beautifulsoup' in terminal
-# Python requires us to explicitly load the libraries that we want to use:
 import requests
 import bs4
 import re
 import pandas as pd
 
 
+# Load a webpage into python so that we can parse it and manipulate it.
+
 # In[15]:
 
 
-# Load a webpage into python so that we can parse it and manipulate it.
+
 URL = 'https://www.espn.com/nba/team/roster/_/name/atl/atlanta-hawks'
 
+
+# Control of Connection. We just turned the website code into a Python object. 
 
 # In[16]:
 
 
-# Control of Connection
-# We just turned the website code into a Python object. 
 response = requests.get(URL)
 soup = bs4.BeautifulSoup(response.text, "html.parser")
 
 
+# Find all the tags with class city or number
+
 # In[17]:
 
 
-# find all the tags with class city or number
+
 data = soup.findAll(attrs={'class':['inline']})
 
 
-# In[18]:
+# Open new file, make sure path to your data file is correct.
+# 
+# Later, I write headers
+
+# In[ ]:
 
 
-f = open('hilca_nba_team_roster.csv','w') # open new file, make sure path to your data file is correct
-f.write("Name\tPos\tAge\tHT\tWT\tCollege\tSalary" + "\n") # write headers
+f = open('hilca_nba_team_roster.csv','w') 
+f.write("Name\tPos\tAge\tHT\tWT\tCollege\tSalary" + "\n")
 
 
 # In[19]:
@@ -112,3 +125,9 @@ for item in results:
 
 f.close() # close file
 
+
+# ## Conclusiones
+# 
+# We used Beautiful Soup as the main tool. The major concept with Beautiful Soup is that it allows you to access elements of your page by following the CSS structures, such as grabbing all links, all headers, specific classes, or more. It is a powerful library.
+# 
+#  Once we grab elements, Python makes it easy to write the elements or relevant components of the elements into other files, such as a CSV, that can be stored in a database or opened in other software.
